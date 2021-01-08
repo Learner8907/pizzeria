4a700eb38ec535704474bbfe2e2273e42d49e044
Learner8907 <72011994+Learner8907@users.noreply.github.com>
2021-01-08 14:17:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
-var menu= ["Margherita Pizza", "Paneer Tikka Pizzza", "Pasta Pizza", "Vegetable Pizza"] 
+var menu= ["Margherita Pizza", "Paneer Tikka Pizzza", "Pasta Pizza", "Vegetable Pizza","Veg Supreme Pizza"] 
 
 function getmenu(){
 
-    for (var i=1; i<5; i++){
-        var id= menu+i;
-        var name= document.getElementById(id).value+"<br><br><br>"
 
-        document.getElementById(display).innerHTML= name;
+var htmldata="";
+htmldata= "<ol class= 'menulist'>"
+menu.sort();
+for(var i=0; i<menu.length; i++){
 
-    }
+    htmldata=htmldata+ "<li>" +menu[i] + "</li>"
 }
+htmldata= htmldata+"</ol>"
+document.getElementById("display").innerHTML= htmldata;
+console.log(htmldata);
+
+
+}
+
+function add_item(){
+    var data=""
+    var item= document.getElementById("add_item").value;
+    menu.push(item);
+    menu.sort();
+   data= '<section class="cards">';
+
+    for(var y=0; y<menu.length;y++){
+        data= data+ '<div class= "card">'+ '<img src="pizzaImg.png">' + menu[y]+"</div>";
+    }
+data=data+"</section>";
+document.getElementById("idkwhatisthetitle").innerHTML= data;
+console.log(data)
+}
+
+
+
 
 
 
